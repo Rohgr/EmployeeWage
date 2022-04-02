cf0d806549a9c23017dd2379fc5c8b3e9611649f
rohgr <rdagrawal6@gmail.com>
2022-04-02 20:37:25 +0530

    Added Uc2EmployeWage

--- src/blz/com/emplyoee/Employee.java
+++ src/blz/com/emplyoee/Employee.java
@@ -2,12 +2,22 @@ package blz.com.emplyoee;
 
 public class Employee {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
-        double check = Math.floor(Math.random() * 10) % 2;
-        System.out.println("Check " +check);
-        if (check == IS_FULL_TIME)
-            System.out.println("Employee is Present");
+        Employ();
+    }
+
+    static void Employ() {
+        int IS_PRESENT = 1;
+        int WAGE_PER_HOUR = 20;
+        int empHrs;
+        int empwage;
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck == IS_PRESENT)
+            empHrs = 8;
         else
-            System.out.println("Employee is Absent");
+            empHrs = 0;
+        empwage = empHrs * WAGE_PER_HOUR;
+        System.out.println("Employee Wage:" + empwage);
     }
 }
+
+
