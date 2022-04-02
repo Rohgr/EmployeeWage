f56fb35b6bdc5451738bd1bc2cfd049cc31c18dc
rohgr <rdagrawal6@gmail.com>
2022-04-02 20:42:00 +0530

    Uc3PartTimeEmployee

--- src/blz/com/emplyoee/Employee.java
+++ src/blz/com/emplyoee/Employee.java
@@ -8,11 +8,15 @@ public class Employee {
     static void Employ() {
         int IS_PRESENT = 1;
         int WAGE_PER_HOUR = 20;
+        int IS_PART_TIME = 2;
         int empHrs;
         int empwage;
-        double empcheck = Math.floor(Math.random() * 10) % 2;
+
+        double empcheck = Math.floor(Math.random() * 10) % 3;
         if (empcheck == IS_PRESENT)
             empHrs = 8;
+        else if (empcheck == IS_PART_TIME)
+            empHrs = 4;
         else
             empHrs = 0;
         empwage = empHrs * WAGE_PER_HOUR;
