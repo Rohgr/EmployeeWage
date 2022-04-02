e3bf7d0e6c25f807561b0181dc57e8ff4de10522
rohgr <rdagrawal6@gmail.com>
2022-04-02 17:04:25 +0530

    Added Uc1EmployeePresentOrAbsent

--- src/blz/com/emplyoee/Employee.java
+++ src/blz/com/emplyoee/Employee.java
@@ -2,6 +2,12 @@ package blz.com.emplyoee;
 
 public class Employee {
     public static void main(String[] args) {
-        System.out.println("Welcome to EmployeWage program ");
+        int IS_FULL_TIME = 1;
+        double check = Math.floor(Math.random() * 10) % 2;
+        System.out.println("Check " +check);
+        if (check == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
