acff52bba467ed1c8250a338f064db9fb8bee4e0
rohgr <rdagrawal6@gmail.com>
2022-04-03 14:15:54 +0530

    Uc8EmployeWageMultipleCompanies

--- src/blz/com/emplyoee/Employee.java
+++ src/blz/com/emplyoee/Employee.java
@@ -6,29 +6,34 @@ public class Employee {
     public static final int WAGE_PER_HOUR = 20;
 
     public static void main(String[] args) {
-        computewage();
+        computewage("Ril", 10, 10, 50);
+        computewage("Dmart", 25, 10, 20);
     }
-        public static int computewage(){
+
+    public static int computewage(String name, int empRatePerHr, int numOfDays, int maxHrs) {
         int salary;
-        int totalhrs = 0;
-        int totalwage = 0;
-        for (int day = 1; day <= 20; day++) {
+        int totalHrs = 0;
+        int totalWage = 0;
+        int totalDays = 0;
+        for (int day = 1; day <= numOfDays; day++) {
             int random = (int) Math.floor(Math.random() * 10) % 3;
             int empHrs = switch (random) {
                 case IS_PART_TIME -> 4;
                 case IS_PRESENT -> 8;
                 default -> 0;
             };
-            totalhrs = totalhrs + empHrs;
-            salary = (empHrs * WAGE_PER_HOUR);
-            totalwage = totalwage + salary;
-            if (totalhrs >= 100) {
-                System.out.println("Salary is:" + totalwage);
+            totalHrs = totalHrs + empHrs;
+            totalDays = totalDays + day;
+            totalWage = totalHrs * empRatePerHr;
+
+            System.out.println("Day#: " + day + " Emp Hr: " + empHrs);
+            if (totalHrs >= maxHrs) {
+                System.out.println("Total Emp Wage for Company: " + name + " is " + totalWage);
                 System.exit(0);
             }
         }
-        System.out.println("Salary is:" + totalwage);
-        return totalwage;
+        System.out.println("Total Emp Wage for Company: " + name + " is " + totalWage);
+        return totalWage;
     }
 }
 
