a2ce299757ddd32af96269011d81165693b81e10
rohgr <rdagrawal6@gmail.com>
2022-04-03 13:14:58 +0530

    Uc5EmployeWageFor20Days

--- src/blz/com/emplyoee/Employee.java
+++ src/blz/com/emplyoee/Employee.java
@@ -6,15 +6,19 @@ public class Employee {
     public static final int WAGE_PER_HOUR = 20;
 
     public static void main(String[] args) {
-        int random = (int) Math.floor(Math.random() * 10) % 3;
-        System.out.println(random);
-        int empHrs = switch (random) {
-            case IS_PART_TIME -> 4;
-            case IS_PRESENT -> 8;
-            default -> 0;
-        };
-        int salary = (empHrs * WAGE_PER_HOUR);
-        System.out.println("Salary is:" + salary);
+        int salary ;
+        int totalwage = 0;
+        for (int day = 1; day <= 20; day++) {
+            int random = (int) Math.floor(Math.random() * 10) % 3;
+            int empHrs = switch (random) {
+                case IS_PART_TIME -> 4;
+                case IS_PRESENT -> 8;
+                default -> 0;
+            };
+            salary = (empHrs * WAGE_PER_HOUR);
+            totalwage = totalwage + salary;
+        }
+        System.out.println("Salary is:" + totalwage);
     }
 }
 
