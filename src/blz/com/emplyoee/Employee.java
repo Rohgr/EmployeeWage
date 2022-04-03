711d953da8a9c16a20a7e2aa9c51322d7b0646db
rohgr <rdagrawal6@gmail.com>
2022-04-03 12:32:29 +0530

    Uc4EmployeSwitchStatmemt

--- src/blz/com/emplyoee/Employee.java
+++ src/blz/com/emplyoee/Employee.java
@@ -1,26 +1,20 @@
 package blz.com.emplyoee;
 
 public class Employee {
+    public static final int IS_PRESENT = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
+
     public static void main(String[] args) {
-        Employ();
-    }
-
-    static void Employ() {
-        int IS_PRESENT = 1;
-        int WAGE_PER_HOUR = 20;
-        int IS_PART_TIME = 2;
-        int empHrs;
-        int empwage;
-
-        double empcheck = Math.floor(Math.random() * 10) % 3;
-        if (empcheck == IS_PRESENT)
-            empHrs = 8;
-        else if (empcheck == IS_PART_TIME)
-            empHrs = 4;
-        else
-            empHrs = 0;
-        empwage = empHrs * WAGE_PER_HOUR;
-        System.out.println("Employee Wage:" + empwage);
+        int random = (int) Math.floor(Math.random() * 10) % 3;
+        System.out.println(random);
+        int empHrs = switch (random) {
+            case IS_PART_TIME -> 4;
+            case IS_PRESENT -> 8;
+            default -> 0;
+        };
+        int salary = (empHrs * WAGE_PER_HOUR);
+        System.out.println("Salary is:" + salary);
     }
 }
 
